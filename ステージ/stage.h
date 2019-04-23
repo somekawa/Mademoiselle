9db25e6d9b51a3743b86a0a257e4739102130164
Mia <Nonaka>
2019-04-23 12:23:24 +0900

    sute

---
+++ ステージ/stage.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define CHIP_SIZE_X 32
+#define CHIP_SIZE_Y 32
+
+#define MAP_X 60
+#define MAP_Y 14
+
+
+void StageSystmInit(void);
+void StageGameInit(void);
+void StageControl(void);
+void StageDraw(void);
+
+XY MapPosToIndex(XY pos);
+bool IsPass(XY pos);
+
+XY MapIndexToPos(XY index);
+XY MapPos(XY pos, MOVE_DIR der);
