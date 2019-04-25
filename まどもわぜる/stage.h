983fdb3c71905fc60a6e58796a1930498c15c9ee
峯 <mine>
2019-04-25 15:21:20 +0900

    stage

--- まどもわぜる/stage.h
+++ まどもわぜる/stage.h
@@ -3,8 +3,10 @@
 #define CHIP_SIZE_X 32
 #define CHIP_SIZE_Y 32
 
-#define MAP_X 50
-#define MAP_Y 14
+#define MAP_X 39
+//#define MAP_X 50
+
+#define MAP_Y 26
 //#define MAP_Y 30
 
 //#define lpstage stage::GetInstance()
